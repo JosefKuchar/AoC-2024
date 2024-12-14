1a2896ff641b25cf75e9c352e181d9fcb398b76b
Josef Kuchař <josef.kuchar267@gmail.com>
2024-12-14 11:28:46 +0100

    feat: day 14

--- README.md
+++ README.md
@@ -41,8 +41,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 11](./src/bin/11.rs) | `238.7µs` | `12.7ms` |
 | [Day 12](./src/bin/12.rs) | `7.5ms` | `8.2ms` |
 | [Day 13](./src/bin/13.rs) | `363.7µs` | `344.0µs` |
+| [Day 14](./src/bin/14.rs) | `245.3µs` | `127.9ms` |
 
-**Total: 465.58ms**
+**Total: 593.73ms**
 <!--- benchmarking table --->
 
 ---
---
+++ src/bin/14.rs
@@ -0,0 +1,115 @@
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
+
+use regex::Regex;
+
+advent_of_code::solution!(14);
+
+#[derive(Debug)]
+struct Robot {
+    position: (i32, i32),
+    velocity: (i32, i32),
+}
+
+fn parse(input: &str) -> Vec<Robot> {
+    let re = Regex::new(r"-?\d+").unwrap();
+    input
+        .lines()
+        .map(|line| {
+            let mut numbers = re
+                .find_iter(line)
+                .map(|m| m.as_str().parse::<i32>().unwrap());
+            Robot {
+                position: (numbers.next().unwrap(), numbers.next().unwrap()),
+                velocity: (numbers.next().unwrap(), numbers.next().unwrap()),
+            }
+        })
+        .collect()
+}
+
+pub fn solve_part_one(input: &str, width: i32, height: i32) -> Option<u32> {
+    let mut robots = parse(input);
+
+    for _ in 0..100 {
+        for robot in robots.iter_mut() {
+            robot.position.0 += robot.velocity.0;
+            robot.position.0 = robot.position.0.rem_euclid(width);
+            robot.position.1 += robot.velocity.1;
+            robot.position.1 = robot.position.1.rem_euclid(height);
+        }
+    }
+
+    let mut quadrants: HashMap<(Ordering, Ordering), usize> = HashMap::new();
+    for robot in robots.iter() {
+        let (x, y) = robot.position;
+        let (xo, yo) = (x.cmp(&(width / 2)), y.cmp(&(height / 2)));
+        if xo == Ordering::Equal || yo == Ordering::Equal {
+            continue;
+        }
+        quadrants
+            .entry((xo, yo))
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+    }
+
+    Some(quadrants.iter().fold(1, |acc, (_, v)| acc * v) as u32)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    solve_part_one(input, 101, 103)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let width = 101;
+    let height = 103;
+    let mut seconds = 0;
+    let mut robots = parse(input);
+
+    let mut symetric_max_count = 0;
+    for i in 0..10000 {
+        for robot in robots.iter_mut() {
+            robot.position.0 += robot.velocity.0;
+            robot.position.0 = robot.position.0.rem_euclid(width);
+            robot.position.1 += robot.velocity.1;
+            robot.position.1 = robot.position.1.rem_euclid(height);
+        }
+
+        let robot_positions: HashSet<(i32, i32)> = robots.iter().map(|r| r.position).collect();
+        let symetric = robot_positions
+            .iter()
+            .filter(|(x, y)| robot_positions.contains(&(width - 1 - x, *y)))
+            .count();
+        if symetric > symetric_max_count {
+            symetric_max_count = symetric;
+            seconds = i + 1;
+            // for y in 0..height {
+            //     for x in 0..width {
+            //         let mut found = false;
+            //         for robot in robots.iter() {
+            //             if robot.position == (x, y) {
+            //                 found = true;
+            //                 break;
+            //             }
+            //         }
+            //         print!("{}", if found { '#' } else { '.' });
+            //     }
+            //     println!();
+            // }
+            // println!("Seconds: {}", i + 1);
+        }
+    }
+    Some(seconds)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = solve_part_one(&advent_of_code::template::read_file("examples", DAY), 11, 7);
+        assert_eq!(result, Some(12));
+    }
+}
